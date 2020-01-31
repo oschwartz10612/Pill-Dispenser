27ada6f1a01ace6c934ac061152a33d60bc0327d
Schwartz <1439726@fcps.edu>
2020-01-31 07:48:18 -0500

    Added Libraries and Intiaized sensor
    Initialized PlatformIO and installed Adafruit Library
    Added sensor iltializeation to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Stepper.h>
+#include "Adafruit_VCNL4010.h"
+
+Adafruit_VCNL4010 proximity;
+
 // Define number of steps per rotation:
 const int stepsPerRevolution = 2048;
 
@@ -6,9 +10,16 @@ Stepper myStepper = Stepper(stepsPerRevolution, 3, 5, 4, 6);
 
 void setup() {
 
-  myStepper.setSpeed(5);
-
+ 
   Serial.begin(9600);
+
+   if (! vcnl.begin()){
+    Serial.println("Sensor not found :(");
+    while (1);
+  }
+  Serial.println("Found VCNL4010");
+  Serial.println("Initializing stepper");
+  myStepper.setSpeed(5);
 }
 void loop() {
   // Step one revolution in one direction:
@@ -20,4 +31,7 @@ void loop() {
   Serial.println("counterclockwise");
   myStepper.step(-stepsPerRevolution);
   delay(500);
+
+  Serial.print("Ambient: "); Serial.println(vcnl.readAmbient());
+  Serial.print("Proximity: "); Serial.println(vcnl.readProximity());
 }
