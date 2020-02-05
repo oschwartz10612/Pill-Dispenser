799fb8b6223e7b0d0c3676095fbe8ffbe6b31fbd
Schwartz <1439726@fcps.edu>
2020-02-05 07:40:13 -0500

    Activation and Finialization
    Added refernce proximity measurement and comparison to deturmine when to
    activate stepper

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,46 @@
 Adafruit_VCNL4010 proximity;
 
 // Define number of steps per rotation:
-const int stepsPerRevolution = 2048;
+const int stepsPerRevolution = 512;
+int prevProx;
+int prevAmb;
+int counter = 0;
 
 Stepper myStepper = Stepper(stepsPerRevolution, 3, 5, 4, 6);
 
 void setup() {
-
- 
   Serial.begin(9600);
 
    if (! proximity.begin()){
     Serial.println("Sensor not found :(");
-    //while (1);
+    while (1);
   } else {
     Serial.println("Found vcnl4010");
   }
 
   Serial.println("Initializing stepper");
   myStepper.setSpeed(10);
+  prevProx = proximity.readProximity();
 }
 void loop() {
-  // Step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
-  
-  // Step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
 
-  // Serial.print("Ambient: "); Serial.println(proximity.readAmbient());
-  // Serial.print("Proximity: "); Serial.println(proximity.readProximity());
+  int prox = proximity.readProximity();
+  Serial.print("Proximity: "); Serial.println(prox);
+
+  int proxDiff = abs(prox - prevProx);
+  Serial.println(proxDiff);
+  if (proxDiff >= 100) {
+    Serial.println("Dispensing...");
+    myStepper.step(stepsPerRevolution);
+
+    delay(5000);
+  }
+
+  counter++;
+  if (counter >= 12) {
+    prevProx = prox;
+    counter = 0;
+  }
+  
+  delay(100);
 }
